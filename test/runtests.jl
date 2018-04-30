f08453b41add877dd204c373f7158efef36710d9
laschuet <laschuet@users.noreply.github.com>
2018-04-30 18:33:03 +0200

    Compute information gain

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,9 @@ header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
 ent = entropy(cars, 7)
 @test ent >= 0 && ent <= 1
 println(ent)
+
+for i in 1:6
+    ig = information_gain(cars, i, 7)
+    @test ig >= 0 && ig <= 1
+    println("ig $(header[i]) $ig")
+end
