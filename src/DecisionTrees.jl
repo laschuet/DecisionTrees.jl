e5b665c54b7a5f593f9b1e6145fda6c0f64ae4d3
laschuet <laschuet@users.noreply.github.com>
2018-04-30 15:17:18 +0200

    Compute entropy

--- src/DecisionTrees.jl
+++ src/DecisionTrees.jl
@@ -1,3 +1,15 @@
 __precompile__()
 module DecisionTrees
+
+using StatsBase
+
+export entropy
+
+"""Compute the entropy of the specified dataset."""
+function entropy(dataset::Array{Any, 2}, target::Int)
+    class = dataset[:, target]
+    prop_vals = collect(values(proportionmap(class)))
+    return -mapreduce(p -> p * log(p), +, 0, prop_vals)
+end
+
 end # module DecisionTrees
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
 using DecisionTrees
 using Base.Test
+
+cars = readdlm(joinpath(Pkg.dir("DecisionTrees"), "test/data/car.data"), ',')
+println(cars)
+
+header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
+
+ent = entropy(cars, 7)
+@test ent >= 0 && ent <= 1
+println(ent)
