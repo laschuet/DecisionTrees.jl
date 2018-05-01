e73af4c8f1951e2e0a87f4ebb4470ece91b210b2
laschuet <laschuet@users.noreply.github.com>
2018-05-01 13:47:00 +0200

    Fix availability of global test variables

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,10 @@
 using DecisionTrees
 using Base.Test
 
+cars = readdlm(joinpath(Pkg.dir("DecisionTrees"), "test/data/car.data"), ',')
+header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
+
 @testset "examplary car data" begin
-    cars = readdlm(joinpath(Pkg.dir("DecisionTrees"), "test/data/car.data"),
-            ',')
-    header = ["buying", "maint", "doors", "persons", "lug_boot", "safety",
-            "class"]
     @test size(cars, 1) == 1728
     @test size(cars, 2) == length(header) == 7
 
