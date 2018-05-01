ffd49b1f9cfa812d79c7d5285d9035d15d707a17
laschuet <laschuet@users.noreply.github.com>
2018-05-01 16:56:02 +0200

    Remove empty docstrings

--- src/measures.jl
+++ src/measures.jl
@@ -5,7 +5,6 @@ function entropy(dataset::Array{Any, 2}, target::Int)
     return -mapreduce(p -> p * log(p), +, 0, prop_vals)
 end
 
-""""""
 entropy(dataset::Array{Any, 2}, target::String, header::Array{String}) =
         entropy(dataset, findfirst(header, target))
 
@@ -23,18 +22,13 @@ function information_gain(dataset::Array{Any, 2}, attribute::Int, target::Int)
     return entropy(dataset, target) - attr_sub_entropy
 end
 
-""""""
 information_gain(dataset::Array{Any, 2}, attribute::String, target::String,
         header::Array{String}) =
                 information_gain(dataset, findfirst(header, attribute),
                         findfirst(header, target))
-
-""""""
 information_gain(dataset::Array{Any, 2}, attribute::String, target::Int,
         header::Array{String}) =
                 information_gain(dataset, findfirst(header, attribute), target)
-
-""""""
 information_gain(dataset::Array{Any, 2}, attribute::Int, target::String,
         header::Array{String}) =
                 information_gain(dataset, attribute, findfirst(header, target))
