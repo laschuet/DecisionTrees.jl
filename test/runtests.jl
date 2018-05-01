890f8ee3460eefbaa4baf15a48ddc08c0ba6ed8b
laschuet <laschuet@users.noreply.github.com>
2018-05-01 13:47:52 +0200

    Move display of loaded data

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,11 @@ using Base.Test
 
 cars = readdlm(joinpath(Pkg.dir("DecisionTrees"), "test/data/car.data"), ',')
 header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
+display(cars)
 
 @testset "examplary car data" begin
     @test size(cars, 1) == 1728
     @test size(cars, 2) == length(header) == 7
-
-    display(cars)
 end
 
 include("measures.jl")
