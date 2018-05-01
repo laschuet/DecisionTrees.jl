5269741187e3d6555dfdea9ad1cace2069597e9d
laschuet <laschuet@users.noreply.github.com>
2018-05-01 15:38:09 +0200

    Remove remaining prints

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,8 @@ using Base.Test
 
 cars = readdlm(joinpath(Pkg.dir("DecisionTrees"), "test/data/car.data"), ',')
 header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
-display(cars)
-println()
 
-@testset "examplary car data" begin
+@testset "data" begin
     @test size(cars, 1) == 1728
     @test size(cars, 2) == length(header) == 7
 end
