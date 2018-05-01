0c7f8c8bae7d9526428a531cbecc3542c50a0474
laschuet <laschuet@users.noreply.github.com>
2018-05-01 11:15:10 +0200

    Structure tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,23 +6,30 @@ println(cars)
 
 header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
 
-ent = entropy(cars, 7)
-@test ent >= 0 && ent <= 1
-println("entr=$ent")
-
-for i in 1:6
-    ig = information_gain(cars, i, 7)
-    @test ig >= 0 && ig <= 1
-    println("ig $(header[i])=$ig")
+@testset "entropy" begin
+    ent = entropy(cars, 7)
+    @test ent >= 0 && ent <= 1
+    println("entr=$ent")
 end
 
-root = Node(header[1], ent, information_gain(cars, 1, 7), [])
-println(root)
+@testset "information_gain" begin
+    for i in 1:6
+        ig = information_gain(cars, i, 7)
+        @test ig >= 0 && ig <= 1
+        println("ig $(header[i])=$ig")
+    end
+end
 
-println(root.children)
-push!(root, root)
-println(root.children)
+@testset "node" begin
+    ent = entropy(cars, 7)
+    root = Node(header[1], ent, information_gain(cars, 1, 7), [])
+    println(root)
 
-println(root.children)
-append!(root, [root, root])
-println(root.children)
+    println(root.children)
+    push!(root, root)
+    println(root.children)
+
+    println(root.children)
+    append!(root, [root, root])
+    println(root.children)
+end
