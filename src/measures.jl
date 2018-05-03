70f529c582b2d6e0d67ae6c2a34e0060b1222cc3
laschuet <laschuet@users.noreply.github.com>
2018-05-03 14:11:10 +0200

    Optionally scale entropy

--- src/measures.jl
+++ src/measures.jl
@@ -1,38 +1,40 @@
 """Compute the entropy of the specified dataset."""
-function entropy(dataset::AbstractArray{T, 2}, target::Int) where {T}
+function entropy(dataset::AbstractArray{T, 2}, target::Int,
+                c::Integer=2) where {T}
     class = dataset[:, target]
     prop_vals = collect(values(proportionmap(class)))
-    return -mapreduce(p -> p * log(p), +, 0, prop_vals)
+    return -mapreduce(p -> p * log2(p), +, 0, prop_vals) / log2(c)
 end
 
 entropy(dataset::AbstractArray{T, 2}, target::String,
-        header::AbstractArray{String}) where {T} =
-    entropy(dataset, findfirst(header, target))
+        header::AbstractArray{String}, c::Integer=2) where {T} =
+    entropy(dataset, findfirst(header, target), c)
 
 """Compute the information gain of the specified attribute."""
 function information_gain(dataset::AbstractArray{T, 2}, attribute::Int,
-                        target::Int) where {T}
+                        target::Int, c::Integer=2) where {T}
     n = size(dataset, 1)
     attr = dataset[:, attribute]
     uniques = unique(attr)
 
     attr_sub_entropy = mapreduce(u -> begin
         subset = dataset[attr .== u, :]
-        return size(subset, 1) / n * entropy(subset, target)
+        return size(subset, 1) / n * entropy(subset, target, c)
     end, +, 0, uniques)
 
     return entropy(dataset, target) - attr_sub_entropy
 end
 
 information_gain(dataset::AbstractArray{T, 2}, attribute::String,
-                target::String, header::AbstractArray{String}) where {T} =
+                target::String, header::AbstractArray{String},
+                c::Integer=2) where {T} =
     information_gain(dataset, findfirst(header, attribute),
-                    findfirst(header, target))
+                    findfirst(header, target), c)
 
 information_gain(dataset::AbstractArray{T, 2}, attribute::String, target::Int,
-                header::AbstractArray{String}) where {T} =
-    information_gain(dataset, findfirst(header, attribute), target)
+                header::AbstractArray{String}, c::Integer=2) where {T} =
+    information_gain(dataset, findfirst(header, attribute), target, c)
 
 information_gain(dataset::AbstractArray{T, 2}, attribute::Int, target::String,
-                header::AbstractArray{String}) where {T} =
-    information_gain(dataset, attribute, findfirst(header, target))
+                header::AbstractArray{String}, c::Integer=2) where {T} =
+    information_gain(dataset, attribute, findfirst(header, target), c)
--- test/measures.jl
+++ test/measures.jl
@@ -1,6 +1,6 @@
 @testset "entropy" begin
-    ent = entropy(cars, 7)
-    ent2 = entropy(cars, "class", header)
+    ent = entropy(cars, 7, 4)
+    ent2 = entropy(cars, "class", header, 4)
     @test ent >= 0 && ent <= 1
     @test ent2 >= 0 && ent2 <= 1
     @test ent == ent2
@@ -8,10 +8,10 @@ end
 
 @testset "information gain" begin
     for i in 1:6
-        ig = information_gain(cars, i, 7)
-        ig2 = information_gain(cars, header[i], "class", header)
-        ig3 = information_gain(cars, header[i], 7, header)
-        ig4 = information_gain(cars, i, "class", header)
+        ig = information_gain(cars, i, 7, 4)
+        ig2 = information_gain(cars, header[i], "class", header, 4)
+        ig3 = information_gain(cars, header[i], 7, header, 4)
+        ig4 = information_gain(cars, i, "class", header, 4)
         @test ig >= 0 && ig <= 1
         @test ig2 >= 0 && ig2 <= 1
         @test ig3 >= 0 && ig3 <= 1
