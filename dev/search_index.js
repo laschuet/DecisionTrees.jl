8f565072bb59bdfb1db2df0e4c83608566fbbe12
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-12 15:57:41 +0000

    build based on a17cbf1

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#DecisionTrees.jl-1",
+    "page": "Home",
+    "title": "DecisionTrees.jl",
+    "category": "section",
+    "text": ""
+},
+
+]}
